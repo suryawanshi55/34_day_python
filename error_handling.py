893f3cf998bfcd436b879f7ae2e12bc747798a2c
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-14 15:12:36 +0530

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,138 @@
+# <.....part-1....>
+# print('python')               #python
+# print("core python")          # core python
+# try:
+#     print(5/0)
+# except:
+#     print('you have divided by zero')       # you have divided by zero
+# print("Advance Python")         #Advance Python
+# print("Python Analytics")      # Python Analytics
+
+# <...part-2...>
+# print("python")   # python
+# print("core python")     #core python
+# try:
+#     print(10/5)     # 2.0
+# except:
+#     print('not ok divided by zero')
+# print('Advanced Python')         # Advanced Python
+# print('python analytics')        # python analytics
+
+# <....part-3...>
+# print("python")         #  python
+# print('core python')     #core python
+# a=5
+# try:
+#     print(b)               #print(b)
+# except ZeroDivisionError:                  #NameError: name 'b' is not defined
+#     print('not ok divided by zero')
+# print("Advanced Python")
+# print("python analytics")
+
+# <...part-4...>
+# print("python")     #python
+# print('core python')     #core python
+# a=5
+# try:
+#     print(a/5)      #1.0
+#     print(b)        #print(b)
+# except ZeroDivisionError:             #NameError: name 'b' is not defined
+#     print('not ok divided by zero')
+# print("Advanced Python")
+# print("python analytics")
+
+# # <...part-5...>
+# print("python")     #python
+# print('core python')    #core python
+# a=5
+# try:
+#     print(a/5)    #1.0
+#     print(a/0)
+# except ZeroDivisionError:
+#     print('not ok divided by zero')   # not ok divided by zero
+# print("Advanced Python")         #Advanced Python
+# print("python analytics")       # python analytics
+
+# # <...part-6...>
+# print("python")     #python
+# print('core python')   # core python
+# a=5
+# try:
+#     print(a/5)     #1.0
+#     print(b)     
+# except ZeroDivisionError:
+#     print('not ok divided by zero')
+# except NameError:
+#     print('you have not defined the name')    #you have not defined the name
+# print("Advanced Python")      #Advanced Python
+# print("python analytics")     #python analytics
+
+# # <....part-7...>
+# print("python")          # python
+# print('core python')     # core python
+# a=5
+# b='6'
+# try:
+#     print(a/5)          #1.0
+#     print(a)           # 5
+#     print(a/0)
+# except ZeroDivisionError:
+#     print('not ok divided by zero')      # not ok divided by zero
+# print("Advanced Python")            # Advanced Python
+# print("python analytics")           #python analytics
+
+# # <...part-8...>
+# print("python")              #python
+# print('core python')          #  core python
+# a=5
+# b='6'
+# try:
+#     print(a/5)             # 1.0
+#     print(a/0)
+#     print(a+"hello")
+# except ZeroDivisionError:
+#     print('not ok divided by zero')    #not ok divided by zero
+# print("Advanced Python")         # Advanced Python
+# print("python analytics")       # python analytics
+
+# # <...part-9...>
+# print("python")     #python
+# print('core python')   #core python
+# a=5
+# b='6'
+# try:
+#     print(a/5)       # 1.0
+#     print(a/0)
+#     print(b+"hello")
+# except ZeroDivisionError:
+#     print('not ok divided by zero')   #not ok divided by zero
+# print("Advanced Python")          #  Advanced Python
+# print("python analytics")            #python analytics
+
+# # <...part-10...>
+# print("python")       # python
+# print('core python')  # core python
+# a=5
+# b='6'
+# try:
+#     print(a/5)         #1.0
+#     print(a/0)
+#     print(b+"hello")
+# except ZeroDivisionError:
+#     print('not ok divided by zero')   #not ok divided by zero
+# print("Advanced Python")            # Advanced Python
+# print("python analytics")           #python analytics
+
+# # <...part-11...>
+# print("python")
+# print('core python')
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print(,)
+# except SyntaxError:
+#     print('Not Ok Divided by Zero')     #syntaxError: invalid syntax
+# print("Advanced Python")
+# print("Python Analytics")
